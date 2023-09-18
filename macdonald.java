30d89f19995db83c3ed02ffd3ba32b595a8be4c3
zhouand25 <zhouand25@7hills.org>
2023-09-18 12:41:22 -0400

    Start

---
+++ macdonald.java
@@ -0,0 +1,6 @@
+public class macdonald {
+    public void sing(String animal, String sound) {
+        System.out.println("Old MacDonald had a "+animal+" e i e i o\n"+"And on that farm he had a "+animal+" e i e i o");
+        System.out.println("With a "+sound+" "+sound+" here and a "+sound+" "+sound+" there\n chaos and death are everywhere");
+    }
+}
---
+++ tester.java
@@ -0,0 +1,6 @@
+public class tester {
+    public static void main(String[] args) {
+        macdonald stuff = new macdonald();
+        stuff.sing("Cow", "moo");
+    }
+}
